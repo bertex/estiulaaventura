cc29d7a185ff418fe8c42d8f456d6721c207c2ff
bertex <albert.renom@gmail.com>
2014-12-21 20:48:46 +0100

    Incldue Angular Refactor navigation Refactor banner Refactor footer Refactor carrusel

---
+++ app.js
@@ -0,0 +1,17 @@
+(function() {
+  var app = angular.module('EstiulaAventura', []);
+
+
+  app.controller("TabController", function() {
+    this.tab = 1;
+
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
+    };
+
+    this.setTab = function(setTab) {
+      this.tab = setTab;
+    };
+  });
+
+})();
