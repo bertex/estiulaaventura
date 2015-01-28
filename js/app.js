1e79ff505b41364ccdf109f557be2b2d59001084
albert.renom <albert.renom@BRASILIA.tolkien.fib.upc.es>
2015-01-28 14:29:46 +0100

    New Font, New format. Test new navigation

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
   app.controller("TabController", function() {
 	    this.tab = 1;
 	    this.background = "tot";
+	    this.banner ="img/banner_web.jpg"
 
 	    this.isSet = function(checkTab) {
 	      return this.tab === checkTab;
@@ -11,7 +12,10 @@
 
 	    this.setTab = function(setTab) {
 	      this.tab = setTab;
-	      if (setTab==2) {this.background="petit"};
+	      if ((setTab==2)||(setTab==8)) {
+	    	  this.background="petit";
+	    	  this.banner ="img/banner_fons_petit.jpg";
+	      };
 	    };
 	  });
 })();
