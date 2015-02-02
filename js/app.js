48fd1ddcea8485cc2f06b19e37ad04d931464c91
albert.renom <albert.renom@BRASILIA.tolkien.fib.upc.es>
2015-02-02 12:00:47 +0100

    Update. Automatic backgronund change, and automatic banenr background change

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@
 
   app.controller("TabController", function() {
 	    this.tab = 1;
-	    this.background = "tot";
-	    this.banner ="img/banner_web.jpg"
+	    this.background = "img/fons_petit.jpg";
+	    this.banner = "img/banner_fons_petit.jpg";
 
 	    this.isSet = function(checkTab) {
 	      return this.tab === checkTab;
@@ -12,10 +12,26 @@
 	    
 	    this.setTab = function(setTab) {
 	      this.tab = setTab;
-	      if ((setTab==2)||(setTab==8)) {
-	    	  this.background="petit";
-	    	  this.banner ="img/banner_fons_petit.jpg";
-	      };
+	      if ((setTab==1)||(setTab==7)) {
+	    	  this.background="img/fons_web.jpg";
+	    	  this.banner ="img/banner_web.jpg";
+	      } else if (setTab==14) {
+	    	  this.background="img/fons_activitats.jpg";
+	    	  this.banner ="img/banner_activitats.jpg";
+	      } else if (setTab==20) {
+	    	  this.background="img/fons_tarifes.jpg";
+	    	  this.banner ="img/banner_tarifes.jpg";
+	      }
 	    };
 	  });
+  
+  app.directive('backImg', function(){
+	    return function(scope, element, attrs){
+	        attrs.$observe('backImg', function(value) {
+	            element.css({
+	                'background': 'url(' + value +') no-repeat top center'
+	            });
+	        });
+	    };
+	});
 })();
