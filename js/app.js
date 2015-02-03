8b8f4a034f036ae014b210452fb16995faf2fbab
albert.renom <albert.renom@BRASILIA.tolkien.fib.upc.es>
2015-02-03 09:41:57 +0100

    update banner, background and footer

--- js/app.js
+++ js/app.js
@@ -5,19 +5,26 @@
 	    this.tab = 1;
 	    this.background = "img/fons_petit.jpg";
 	    this.banner = "img/banner_fons_petit.jpg";
+	    this.fons = "col-centre-petit"
 
 	    this.setTab = function(setTab) {
 	      this.tab = setTab;
 	      if ((setTab==1)||(setTab==7)) {
 	    	  this.background="img/fons_web.jpg";
 	    	  this.banner ="img/banner_web.jpg";
-	      } else if (setTab==14) {
+	    	  this.fons="col-centre-portada";
+	      } else if ((setTab>=15)&& (setTab <=18) ) {
 	    	  this.background="img/fons_activitats.jpg";
 	    	  this.banner ="img/banner_activitats.jpg";
-	      } else if (setTab==20) {
+	    	  this.fons="col-centre-activitats"
+	      } else if ((setTab==20) ||(setTab==14)) {
 	    	  this.background="img/fons_tarifes.jpg";
 	    	  this.banner ="img/banner_tarifes.jpg";
+	    	  this.fons = "col-centre-tarifes";
+	      } else if (setTab==21)  {
+	    	  this.fons = "col-centre-tiquet";
 	      }
+	      
 	    };
 	  });
   
