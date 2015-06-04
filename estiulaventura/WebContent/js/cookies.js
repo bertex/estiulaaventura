97aa6c6fcc3f3b0b80f1c96e93b7c1bbe4ef21f6
albert.renom <albert.renom@fib.upc.edu>
2015-06-04 13:28:47 +0200

    Update footer. Better version but not final fix some bugs. Add legal advice, cookie advice and pilicy advice

--- estiulaventura/WebContent/js/cookies.js
+++ estiulaventura/WebContent/js/cookies.js
@@ -2,6 +2,6 @@ window.cookieconsent_options = {
 	"message" : "Aquest pàgina web utilitza cookies per assegurar d'obtenir la millor experiència mentre visites la nostra web",
 	"dismiss" : "Accepta !!",
 	"learnMore" : "Més informació",
-	"link" : null,
+	"link" : "cookies.html",
 	"theme" : "dark-bottom"
 };
