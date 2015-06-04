b62456c5a24064852eabc51a5222d34fcdafe394
albert.renom <albert.renom@fib.upc.edu>
2015-06-04 09:38:23 +0200

    organize scripts. add cookie conscent script

--- estiulaventura/WebContent/js/cookies.js
+++ estiulaventura/WebContent/js/cookies.js
@@ -1,42 +1,7 @@
-// <![CDATA[
-cc.initialise({
-	cookies: {
-		analytics: {}
-	},
-	settings: {
-		consenttype: "implicit",
-		bannerPosition: "bottom"
-	},
-	strings: {
-		preferenceAlways: 'Sempre acceptar',
-		preferenceNever: 'Mai acceptar',
-		preferenceAsk: 'Pregunta-m\'ho cada cop',
-		backToSiteSettings: 'Tornar a la configuració',
-		allSitesSettingsDialogTitleB: 'Per a tota la web',
-		allSitesSettingsDialogTitleA: 'Configuració de la privacitat',
-		allSitesSettingsDialogSubtitle: 'Accepto les cookies de tota la web, que utilitzi aquest plugin',
-		preferenceDecline: 'Rebutjar',
-		preferenceConsent: 'Acceptar',
-		preferenceUseGlobal: 'Utilitzar configuració general',
-		notUsingCookies: 'Aquesta web no utilitza cookies',
-		changeForAllSitesLink: 'Canviar la configuració per a tota la web',
-		privacySettingsDialogTitleB: 'per aquesta web',
-		privacySettingsDialogTitleA: 'Configuració de la privacitat',
-		privacySettings: 'Configuració cookies',
-		analyticsDefaultTitle: 'Analítics',
-		analyticsDefaultDescription: 'De forma anònima recollim com navegas per aquesta web per millora-ne la teva experiència d,\'usuari.',
-		seeDetails: '+ info',
-		hideDetails: 'amaga els detalls',
-		allowCookies: 'Permet cookies',
-		allowCookiesImplicit: 'Tanca',
-		allowForAllSites: 'Permetre per tota la web',
-		savePreference: 'Desa preferències ',
-		saveForAllSites: 'Desa per tota la web',
-		customCookie: 'Aquesta web utilitza un tipus ,de cookies per les quals és ne,cessari la teva aprovació',
-		notificationTitle: 'La teva experiència d\'usuari, en aquesta web, millora si permets l\'ús de cookies.',
-		notificationTitleImplicit: 'Utilitzem cookies per assegurar la millor experiència de la nostra web',
-		closeWindow: 'Tancar finestra',
-		learnMore: '+ info'
-	}
-});
-// ]]>
+window.cookieconsent_options = {
+	"message" : "Aquest pàgina web utilitza cookies per assegurar d'obtenir la millor experiència mentre visites la nostra web",
+	"dismiss" : "Accepta !!",
+	"learnMore" : "Més informació",
+	"link" : null,
+	"theme" : "dark-bottom"
+};
